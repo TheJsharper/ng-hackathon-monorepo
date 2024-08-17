fd23792b3e9868f84bdd85f0c6b765a110b5e5ca
Pedro Salomon <pedro.salomon@gmail.com>
2024-08-17 23:58:32 +0200

    table of product added

--- libs/products/src/lib/products/products.component.ts
+++ libs/products/src/lib/products/products.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { ProductService, SharedServicesModule } from '@ng-hackathon-monorepo/shared-services';
+import { Product } from '@ng-hackathon-monorepo/types';
 
 @Component({
   selector: 'lib-products',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgFor,SharedServicesModule ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss',
 })
-export class ProductsComponent {}
+export class ProductsComponent {
+  private productService: ProductService = inject(ProductService);
+
+   products: Array<Product> = this.productService.getProductsData();;
+}
--- libs/shared-services/src/lib/shared-services.module.ts
+++ libs/shared-services/src/lib/shared-services.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductService } from './services/product.service';
 
 @NgModule({
   imports: [CommonModule],
+  providers:[ProductService]
 })
 export class SharedServicesModule {}
