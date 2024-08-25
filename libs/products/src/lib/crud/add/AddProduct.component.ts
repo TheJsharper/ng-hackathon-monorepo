cb7683c1690d639d3351a51874bcf76ba7ebb2c6
Pedro Salomon <pedro.salomon@gmail.com>
2024-08-25 23:39:46 +0200

    MockApi added

--- libs/products/src/lib/crud/add/AddProduct.component.ts
+++ libs/products/src/lib/crud/add/AddProduct.component.ts
@@ -1,14 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'lib-app-add-product',
   standalone: true,
   imports: [
-    CommonModule,
+    CommonModule, ReactiveFormsModule
   ],
   templateUrl: './AddProduct.component.html',
   styleUrl: './AddProduct.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AddProductComponent { }
+export class AddProductComponent { 
+
+  form:FormGroup;
+    constructor(private fb:FormBuilder){
+      this.form = this.fb.group( {
+        name: new FormControl('')
+      })
+    }
+}
